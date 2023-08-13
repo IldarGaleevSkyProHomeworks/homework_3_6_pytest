2713caef9da7633da32ef249f895a6888cb2a563
Галеев Ильдар <ildar-galeev92@mail.ru>
2023-08-13 22:51:10 +0700

    Added fixture for arrs tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,14 +1,21 @@
+import pytest
+
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 2, "test") == 3
+@pytest.fixture()
+def test_data():
+    return [1, 2, 3]
+
+
+def test_get(test_data):
+    assert arrs.get(test_data, 2, "test") == 3
     assert arrs.get([], -1, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_slice(test_data):
+    assert arrs.my_slice(test_data, 1, 3) == [2, 3]
+    assert arrs.my_slice(test_data, 1) == [2, 3]
     assert arrs.my_slice([1], -1) == [1]
     assert arrs.my_slice([1], -2) == [1]
     assert arrs.my_slice([]) == []
